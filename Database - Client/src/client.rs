16b082d0c5c36959e2860777ad599b7bcbf17309
John McMenemy <john.mcmenemy.2017@uni.strath.ac.uk>
2020-03-23 16:40:29 +0000

    Made new simultaneous test clients

---
+++ Database - Client/src/client.rs
@@ -0,0 +1,156 @@
+#![warn(rust_2018_idioms)]
+
+use std::str;
+use std::io::Error;
+
+use tokio;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+
+use std::time::{Instant};
+
+use rand::{thread_rng, Rng};
+use rand::distributions::{Alphanumeric, Uniform};
+
+
+pub async fn main(addr: String, command: u8) -> Result<(), Box<Error>> {
+
+    // let addr = env::args()
+    //     .nth(1)
+    //     .unwrap_or_else(|| "127.0.0.1:6142".to_string());
+
+    // let addr = "127.0.0.1:6142".to_string();
+
+    let mut socket;
+    match TcpStream::connect(&addr).await{
+        Ok(s)=>{
+            socket = s;
+        }
+        Err(error)=>{
+            return Err(Box::new(error));
+        }
+    }
+
+    // socket.write_all("ç".as_bytes()).await.expect("failed to write data to socket");
+
+    // socket.write_all("resetlog;\n".as_bytes()).await.expect("failed tso write data to socket");
+    //
+    // let response: String = read_from_socket(&mut socket).await;
+    // assert_eq!(true, response.contains("Ok"));
+
+    match command{
+        1 => {
+            short_test_all_commands(&mut socket).await;
+        },
+        2 => {
+            multiple_set_commands(&mut socket, 100).await;
+        },
+        3 =>{
+            change_name(&mut socket, "Name", "Bob").await;
+        },
+        4 =>{
+            change_name(&mut socket, "Name", "Alice").await;
+        }
+        _ =>{}
+    }
+
+    Ok(())
+}
+
+async fn multiple_set_commands(mut socket: &mut TcpStream, number: u128){
+
+    let mut sum_of_times = 0;
+
+    let start_time = Instant::now();
+
+    for _i in 0..number{
+
+        let first_time = Instant::now();
+
+        let range = Uniform::new(5, 100);
+
+        let x = thread_rng().sample(range);
+        let key: String = thread_rng().sample_iter(Alphanumeric).take(x).collect();
+        let x = thread_rng().sample(range);
+        let value: String = thread_rng().sample_iter(Alphanumeric).take(x).collect();
+
+        let command = format!("set {} {};\n", key, value);
+
+
+        socket.write_all(command.as_bytes()).await.expect("failed to write data to socket");
+        let _response: String = read_from_socket(&mut socket).await;
+
+        let second_time = Instant::now();
+        let time_taken = second_time.duration_since(first_time).as_millis();
+        // println!("Iteration {}, time taken {}ms", i, time_taken);
+        sum_of_times += time_taken;
+    }
+
+    let end_time = Instant::now();
+
+    let total_time = end_time.duration_since(start_time).as_secs();
+
+    println!("Client report: Time taken to complete the average test was {}ms. Total time taken {}s", (sum_of_times/number), total_time);
+
+}
+
+async fn short_test_all_commands(mut socket: &mut TcpStream){
+    let mut sum_of_times = 0;
+
+    for _i in 0..10{
+
+        let start_time = Instant::now();
+
+        socket.write_all("set Name Rustacean;\n".as_bytes()).await.expect("failed tso write data to socket");
+
+        let response: String = read_from_socket(&mut socket).await;
+        assert_eq!(true, response.contains("Ok"));
+
+        socket.write_all("get Name;\n".as_bytes()).await.expect("failed to read data from socket");
+        let response: String = read_from_socket(&mut socket).await;
+        assert_eq!(true, response.contains("Rustacean"));
+
+        socket.write_all("remove Name;\n".as_bytes()).await.expect("failed to write data to socket");
+        let response: String = read_from_socket(&mut socket).await;
+        // println!("line 102 response {:?}", response);
+        assert_eq!(true, response.contains("Rustacean"));
+
+        socket.write_all("get Name;\n".as_bytes()).await.expect("failed to read data from socket");
+        let response: String = read_from_socket(&mut socket).await;
+        assert_eq!(true, (response.contains("not found") && response.contains("Name")));
+
+        let end_time = Instant::now();
+
+        // println!("Loop {} time {:?}", i, end_time.duration_since(start_time).as_millis());
+
+        sum_of_times += end_time.duration_since(start_time).as_millis();
+    }
+
+    println!("Client report: Time taken to complete tests is {}", (sum_of_times/10));
+}
+
+async fn change_name(mut socket: &mut TcpStream, key: &str, value: &str){
+    for _i in 0..10{
+        let command = format!("set {} {};\n", key, value);
+        socket.write_all(command.as_bytes()).await.expect("failed to write data to socket");
+        let _response: String = read_from_socket(&mut socket).await;
+        let command = "get Name;\n";
+        socket.write_all(command.as_bytes()).await.expect("failed to write data to socket");
+        let response: String = read_from_socket(&mut socket).await;
+        // println!("Value: {}, response: {}", value, response);
+        assert_eq!(true, response.contains(value));
+    }
+}
+
+async fn read_from_socket(socket: &mut TcpStream) -> String{
+    let mut read_buf = [0; 1024];
+    socket
+        .read(&mut read_buf)
+        .await
+        .expect("failed to read data from socket");
+    let response = str::from_utf8(&read_buf).unwrap();
+    let response =  response.chars()
+        .filter(|c| *c != '\u{0}')
+        .collect::<String>();
+    response
+}
--- Database - Client/src/main.rs
+++ Database - Client/src/main.rs
@@ -1,131 +1,47 @@
-#![warn(rust_2018_idioms)]
-
-use std::str;
-use std::io::Error;
-
-use tokio;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
 
 use std::env;
-use std::time::{Instant};
 
-use rand::{thread_rng, Rng};
-use rand::distributions::{Alphanumeric, Uniform};
+use tokio::runtime::Builder;
 
+mod client;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<Error>> {
+fn main() -> std::result::Result<(), std::boxed::Box<std::io::Error>> {
+
+    // build runtime
+    let mut rt = Builder::new()
+    .threaded_scheduler()
+    .enable_all()
+    .build()
+    .unwrap();
 
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:6142".to_string());
 
-    // let addr = "127.0.0.1:6142".to_string();
+    rt.block_on(async {
+        let client_one = tokio::spawn(client::main(addr.clone(), 3));
+        // let client_two = tokio::spawn(client::main(addr.clone(), 4));
 
-    let mut socket;
-    match TcpStream::connect(&addr).await{
-        Ok(s)=>{
-            socket = s;
+        let client_one = client_one.await.expect("Client handle panicked.");
+        // let client_two = client_two.await.expect("Client handle panicked.");
+        match client_one{
+            Ok(()) => {
+                println!("All tests completed successfully.");
+            },
+            Err(error) => {
+                println!("Some error has occurred!");
+                eprintln!("{:?}", error);
+            }
         }
-        Err(error)=>{
-            return Err(Box::new(error));
-        }
-    }
-
-    // socket.write_all("ç".as_bytes()).await.expect("failed to write data to socket");
-
-    // socket.write_all("resetlog;\n".as_bytes()).await.expect("failed tso write data to socket");
-    //
-    // let response: String = read_from_socket(&mut socket).await;
-    // assert_eq!(true, response.contains("Ok"));
-
-    short_test_all_commands(&mut socket).await;
-    // multiple_set_commands(&mut socket, 1000).await;
-
-    Ok(())
-}
-
-async fn multiple_set_commands(mut socket: &mut TcpStream, number: u128){
-
-    let mut sum_of_times = 0;
-
-    let start_time = Instant::now();
-
-    for _i in 0..number{
-
-        let first_time = Instant::now();
-
-        let range = Uniform::new(5, 100);
-
-        let x = thread_rng().sample(range);
-        let key: String = thread_rng().sample_iter(Alphanumeric).take(x).collect();
-        let x = thread_rng().sample(range);
-        let value: String = thread_rng().sample_iter(Alphanumeric).take(x).collect();
-
-        let command = format!("set {} {};\n", key, value);
-
-
-        socket.write_all(command.as_bytes()).await.expect("failed to write data to socket");
-        let _response: String = read_from_socket(&mut socket).await;
-
-        let second_time = Instant::now();
-        let time_taken = second_time.duration_since(first_time).as_millis();
-        // println!("Iteration {}, time taken {}ms", i, time_taken);
-        sum_of_times += time_taken;
-    }
-
-    let end_time = Instant::now();
-
-    let total_time = end_time.duration_since(start_time).as_secs();
-
-    println!("Client report: Time taken to complete the average test was {}ms. Total time taken {}s", (sum_of_times/number), total_time);
-
-}
-
-async fn short_test_all_commands(mut socket: &mut TcpStream){
-    let mut sum_of_times = 0;
-
-    for _i in 0..10{
-
-        let start_time = Instant::now();
-
-        socket.write_all("set Name Rustacean;\n".as_bytes()).await.expect("failed tso write data to socket");
-
-        let response: String = read_from_socket(&mut socket).await;
-        assert_eq!(true, response.contains("Ok"));
-
-        socket.write_all("get Name;\n".as_bytes()).await.expect("failed to read data from socket");
-        let response: String = read_from_socket(&mut socket).await;
-        assert_eq!(true, response.contains("Rustacean"));
-
-        socket.write_all("remove Name;\n".as_bytes()).await.expect("failed to write data to socket");
-        let response: String = read_from_socket(&mut socket).await;
-        assert_eq!(true, response.contains("Rustacean"));
-
-        socket.write_all("get Name;\n".as_bytes()).await.expect("failed to read data from socket");
-        let response: String = read_from_socket(&mut socket).await;
-        assert_eq!(true, (response.contains("not found") && response.contains("Name")));
-
-        let end_time = Instant::now();
-
-        // println!("Loop {} time {:?}", i, end_time.duration_since(start_time).as_millis());
-
-        sum_of_times += end_time.duration_since(start_time).as_millis();
-    }
-
-    println!("Client report: Time taken to complete tests is {}", (sum_of_times/10));
-}
-
-async fn read_from_socket(socket: &mut TcpStream) -> String{
-    let mut read_buf = [0; 1024];
-    socket
-        .read(&mut read_buf)
-        .await
-        .expect("failed to read data from socket");
-    let response = str::from_utf8(&read_buf).unwrap();
-    let response =  response.chars()
-        .filter(|c| *c != '\u{0}')
-        .collect::<String>();
-    response
+        // match client_two{
+        //     Ok(()) => {
+        //         println!("All tests completed successfully.");
+        //     },
+        //     Err(error) => {
+        //         println!("Some error has occurred!");
+        //         eprintln!("{:?}", error);
+        //     }
+        // }
+        Ok(())
+    })
 }
